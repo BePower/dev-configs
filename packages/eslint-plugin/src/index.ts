c98257af8399e4e367e84a8839c31e73b2111fa0
Niccolò Olivieri Achille <niccolo.olivieri@bepower.com>
2022-07-26 11:23:13 +0200

    refactor(eslint): :fire: Removed the rules file

--- packages/eslint-plugin/src/index.ts
+++ packages/eslint-plugin/src/index.ts
@@ -1,14 +1,13 @@
 import { configs as Configs, rules as Rules } from '@typescript-eslint/eslint-plugin';
 
 import * as configs from './configs';
-import * as rules from './rules';
 
 const config: {
   configs: typeof Configs;
   rules: typeof Rules;
 } = {
   configs,
-  rules,
+  rules: {},
 };
 
 export = config;
