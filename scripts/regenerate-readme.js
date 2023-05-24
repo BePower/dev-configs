e1f9b155f3fc3c9f7cbc386bc36935ecd5ea78e0
Niccolò Olivieri Achille <niccolo.olivieri@bepower.com>
2023-05-24 11:18:09 +0200

    ci: :construction_worker: Migrating to `auto` and `npm@7`

--- scripts/regenerate-readme.js
+++ scripts/regenerate-readme.js
@@ -1,30 +1,50 @@
+const { execSync } = require('child_process');
 const { readdirSync, readFileSync, writeFileSync } = require('fs');
 const { join } = require('path');
 
 const packagesFolder = 'packages';
 const packagesPath = join(__dirname, '..', packagesFolder);
-const packages = readdirSync(packagesPath).map((package) => ({
-  package,
-  version: require(join(packagesPath, package, 'package.json')).version,
-}));
+
 
 const readmePath = join(__dirname, '..', 'README.md');
-const readme = readFileSync(readmePath, 'utf8');
 const startTag = '[//]: # (BEGIN TABLE)';
 const endTag = '[//]: # (END TABLE)';
 
-const [firstPart] = readme.split(startTag);
-const [,lastPart] = readme.split(endTag);
+module.exports = class TestPlugin {
+  constructor() {
+    this.name = 'update-readme';
+  }
 
-const rows = ['| Name | Version | Install command |', '| --- | --- | --- |'];
-rows.push(...packages.map(({ package, version }) => [
-  '| ',
-  `[${package}](${packagesFolder}/${package})`,
-  ' | ',
-  `[![${package}: ${version}](https://img.shields.io/badge/${package.replace(/-/g, '--')}-${version}-brightgreen.svg)](${packagesFolder}/${package}/package.json)`,
-  ' | ',
-  `\`$ npm install --save-dev @bepower/${package}@${version}\``,
-  ' |',
-].join('')));
+  apply(auto) {
+    auto.hooks.beforeCommitChangelog.tap(this.name, ({ lastRelease, bump }) => {
+      const packages = readdirSync(packagesPath)
+        .map((package) => ({
+          package,
+          version: require(join(packagesPath, package, 'package.json')).version,
+        }));
 
-writeFileSync(readmePath, [firstPart, startTag, '\n', '\n', rows.join('\n'), '\n', '\n', endTag, lastPart].join(''));
+      const readme = readFileSync(readmePath, 'utf8');
+
+      const [firstPart] = readme.split(startTag);
+      const [,lastPart] = readme.split(endTag);
+
+      const rows = ['| Name | Version | Install command |', '| --- | --- | --- |'];
+      rows.push(...packages.map(({ package, version }) => [
+        '| ',
+        `[${package}](${packagesFolder}/${package})`,
+        ' | ',
+        `[![${package}: ${version}](https://img.shields.io/badge/${package.replace(/-/g, '--')}-${version}-brightgreen.svg)](${packagesFolder}/${package}/package.json)`,
+        ' | ',
+        `\`$ npm install --save-dev @bepower/${package}@${version}\``,
+        ' |',
+      ].join('')));
+
+      writeFileSync(readmePath, [firstPart, startTag, '\n', '\n', rows.join('\n'), '\n', '\n', endTag, lastPart].join(''));
+
+      execSync(`git add ${readmePath}`)
+    });
+  }
+}
+
+
+
