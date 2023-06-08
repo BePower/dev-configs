4b75dab817d93a40901aa96d18e54e144227d0f0
Niccolò Olivieri Achille <niccolo.olivieri@bepower.com>
2023-06-08 20:29:51 +0200

    ci: :construction_worker: Adding some debug for the custom plugin

--- scripts/regenerate-readme.ts
+++ scripts/regenerate-readme.ts
@@ -1,11 +1,13 @@
-import { Auto, IPlugin, execPromise } from '@auto-it/core';
+import { Auto, IPlugin, execPromise, getLernaPackages } from '@auto-it/core';
 
 export default class RegenerateReadmePlugin implements IPlugin {
   name = 'regenerate-readme';
 
   apply(auto: Auto): void {
     auto.hooks.afterVersion.tapPromise(this.name, async () => {
+      const packages = await getLernaPackages();
 
+      auto.logger.log.debug(packages)
     });
   }
 }
