a37fdd360cb84c85207f0ea42514e7de808f2307
Niccolò Olivieri Achille <niccolo.olivieri@bepower.com>
2023-06-08 16:04:06 +0200

    feat(auto): :tada: Added the `auto-config` package

---
+++ packages/auto-config/auto.config.ts
@@ -0,0 +1,15 @@
+import { ConventionalCommitsOptions } from '@auto-it/conventional-commits';
+import { AutoRc } from 'auto';
+
+const conventionalCommitsOptions: ConventionalCommitsOptions = {
+  preset: 'angular',
+}
+
+export default (): AutoRc => ({
+  plugins: [
+    'npm',
+    'all-contributors',
+    ['conventional-commits', conventionalCommitsOptions],
+    'first-time-contributor',
+  ],
+});
