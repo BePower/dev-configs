f5854454594be6ae31e3f11cd85d706fc6c6a964
Niccolò Olivieri Achille <niccolo.olivieri@bepower.com>
2023-06-15 20:47:01 +0200

    fix: :ambulance: Fixed the import of the renamed packages

--- .autorc.js
+++ .autorc.js
@@ -1,13 +1,13 @@
 const base = require('@bepower/auto-config').default;
 
-const regenerate = require.resolve('@bepower/auto-config/scripts/regenerate-readme');
+const packages = require.resolve('@bepower/auto-config/scripts/add-packages-to-readme');
 const coverage = require.resolve('@bepower/auto-config/scripts/add-coverage-to-readme');
 
 /**
  * @returns {import('@auto-it/core').default}
  */
 function config() {
-  base.plugins.push(regenerate);
+  base.plugins.push(packages);
   base.plugins.push(coverage);
 
   return base;
