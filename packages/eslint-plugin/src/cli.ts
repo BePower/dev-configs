551f85caf1415957ee64b2a5c03f7a12fb062578
Niccolò Olivieri Achille <55181558+NiccoloOlivieriAchille@users.noreply.github.com>
2023-10-11 15:12:58 +0000

    feat(eslint): :sparkles: Adding the cli

---
+++ packages/eslint-plugin/src/cli.ts
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+import { writeFileSync } from 'fs';
+import { join } from 'path';
+
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers';
+
+const eslintrcContent = {
+  extends: 'plugin:@bepower/node',
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+};
+
+const eslintignoreNames = ['cdk.out', 'dist'];
+
+yargs(hideBin(process.argv))
+  .command(
+    'config:init',
+    'Initializes the dot files',
+    () => {},
+    (argv) => {
+      const eslintrcPath = join(process.cwd(), '.eslintrc');
+      writeFileSync(eslintrcPath, JSON.stringify(eslintrcContent, undefined, 2));
+      process.stderr.write(`ESLint configuration written to ${eslintrcPath}\n`);
+
+      if (argv.ignoreFile) {
+        const eslintignorePath = join(process.cwd(), '.eslintignore');
+        writeFileSync(eslintignorePath, eslintignoreNames.join('\n'));
+        process.stderr.write(`ESLint ignore configuration written to ${eslintignorePath}\n`);
+      }
+    },
+  )
+  .option('ignore-file', {
+    alias: 'i',
+    type: 'boolean',
+    description: 'Initialize .eslingignore file too',
+  })
+  .help()
+  .demandCommand(1)
+  .parse();
